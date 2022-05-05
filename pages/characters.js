65a514a057a44d68146265cb170cffa2edf5904e
Eugen Dirksen <eugen@candlesofwisdom.com>
2022-05-05 16:43:12 +0200

    Add fetch and mapping

--- pages/characters.js
+++ pages/characters.js
@@ -1,10 +1,29 @@
 import Card from "../components/Card";
 import CardWrapper from "../components/CardsWrapper";
+import { useEffect, useState } from "react";
 
 export default function CharactersPage() {
+  const [characterList, setCharacterList] = useState([]);
+  useEffect(() => {
+    const url = "https://rickandmortyapi.com/api/character/";
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCharacterList(data.results);
+      });
+  }, []);
+  //---------- characterList was filled with setCharacterList
+  //   useEffect(()=> {
+  //       console.log(characterList);
+  //   },[characterList])
+
   return (
     <CardWrapper>
-      <Card>Character 1</Card>
+      {characterList.map((character) => (
+        <Card key={character.id} name={character.name}>
+          {character.name}
+        </Card>
+      ))}
     </CardWrapper>
   );
 }
