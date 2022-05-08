54d4af162a86846cabc965df741a1be5aa6c03d5
Eugen Dirksen <eugen@candlesofwisdom.com>
2022-05-08 20:48:13 +0200

    Add episodes

--- pages/characters.js
+++ pages/characters.js
@@ -1,17 +1,23 @@
-import Card from "../components/Card";
-import CardsWrapper from "../components/CardsWrapper";
-import Nav from "../components/Nav";
-import useFetch from "../components/useFetch";
+import Card from '../components/Card';
+import CardsWrapper from '../components/CardsWrapper';
+import Nav from '../components/Nav';
+import useFetch from '../components/useFetch';
 
 export default function CharactersPage() {
-  const { data } = useFetch("https://rickandmortyapi.com/api/character/");
-
+  const { data } = useFetch('https://rickandmortyapi.com/api/character/');
   return (
     <>
       <CardsWrapper>
-        {data?.results.map((character) => (
+        {data?.results.map(character => (
           <Card key={character.id} name={character.name}>
             {character.name}
+            <img src={character.image} alt={character.name} />
+            <ul>
+              <li>Status: {character.status}</li>
+              <li>Species: {character.species}</li>
+              <li>Gender: {character.gender}</li>
+              <li>Location: {character.location.name}</li>
+            </ul>
           </Card>
         ))}
       </CardsWrapper>
